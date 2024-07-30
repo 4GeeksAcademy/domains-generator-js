00d10ce79a0c32be640aacfb3791d307620bbc1b
Angel Salazar <146801401+AngelSalazarV@users.noreply.github.com>
2024-07-30 21:24:42 +0000

    Proyect finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+function domainGenerator() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
+}
+
+domainGenerator();
